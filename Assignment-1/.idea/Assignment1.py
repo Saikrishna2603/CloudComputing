492aa2970b7eae872ebb72259f5663c08c81db1d
CloudComputing Repo <msaikrishna2603@gmail.com>
2023-01-25 19:54:44 -0600

    Adding Python Files

--- Assignment-1/.idea/Assignment1.py
+++ Assignment-1/.idea/Assignment1.py
@@ -7,4 +7,4 @@ principal = int(input("Enter The Amount of Loan $"))
 rate = float(input("Enter The Interest rate "))
 time = int(input("Enter The Number of Years "))
 intrest = principal * (rate / 100) * time
-print("The interest on a loan of $", principal, "at", rate, "% intrest rate for ", time, "year \nis $", round(intrest, 2),".");
+print("The interest on a loan of $", principal, "at", rate, "% intrest rate for ", time, "year \nis $", round(intrest, 2),".",sep="");
