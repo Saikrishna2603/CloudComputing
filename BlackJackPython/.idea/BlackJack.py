b82393c71bfcab6a412be5baf83b0f9c8ea3462c
Cloud Computing <msaikrishna2603@gmail.com>
2023-01-28 02:41:07 -0600

    Adding Decks & Main Method

--- BlackJackPython/.idea/BlackJack.py
+++ BlackJackPython/.idea/BlackJack.py
@@ -1,15 +1,12 @@
 import array
 
 
-class BlackJack:
-    DecksObj=Decks(object)
-    cardlist=DecksObj.shuffle()
-class Cards:
-    print()
-class Decks(object):
 
-    @classmethod
-    def shuffle():
+class Cards:
+    def getCard(self):
+        print()
+class Decks:
+    def shuffle(self):
         ranks = ["2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack", "Queen", "King", "Ace"];
         suits = ["Clubs", "Hearts", "Diamonds", "Spades"];
         DeckOfCards = []
@@ -18,3 +15,20 @@ class Decks(object):
                 cardValues = ranks[j] + " of " + suits[i]
                 DeckOfCards.append(cardValues)
         return DeckOfCards;
+
+
+class BlackJack:
+    def main():
+        start= True
+        UpdatedTotal
+        DecksObj=Decks()
+        cardlist=DecksObj.shuffle()
+        CardsObj=Cards()
+        while True:
+            print("Player Side")
+            ReturnCardList=CardsObj.getCard(cardlist,start,ReturnCardList,UpdatedTotal)
+            if
+
+if __name__ == "__main__":
+    bj=BlackJack
+    bj.main()
